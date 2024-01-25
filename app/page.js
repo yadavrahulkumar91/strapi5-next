5a3059284c7a04e5f6ece6200aa998057155fe2f
Rahul Kumar Yadav <yadavrahulkumar91@gmail.com>
2024-01-25 20:50:00 +0545

    failure

--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
 
 import Section from './components/Section.js'
-// import { PhotoProvider, PhotoView } from 'react-photo-view';
-// import 'react-photo-view/dist/react-photo-view.css';
 
 export default function Home() {
  
@@ -10,7 +8,7 @@ export default function Home() {
     
     {/* <main className="flex min-h-screen flex-col items-center justify-between p-24"> */}
     <main className="flex flex-col items-center justify-between align-center">
-
+  
     <Section/>
 
   
